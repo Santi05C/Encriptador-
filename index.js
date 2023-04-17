bdef169a3cb30bbafa3bccd01157c06621366d47
Santi05C <santiicarp17@gmail.com>
2023-04-17 10:51:10 -0400

    last commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function(){
-    
+function encriptado() {
+  let texto = document.getElementById("texto").value;
+
+  let textoCiffrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ea")
+    .replace(/o/gi, "enter")
+    .replace(/u/gi, "enter");
 }
