e0284553435c918780d1f961b616069e5332074c
Santi05C <santiicarp17@gmail.com>
2023-04-17 11:49:44 -0400

    last commit

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-function encriptado() {
+function encriptar() {
   let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
 
-  let textoCiffrado = texto
+  let textoCifrado = texto
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
-    .replace(/a/gi, "ea")
-    .replace(/o/gi, "enter")
-    .replace(/u/gi, "enter");
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    texto = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/encriptado.jpg";
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    alert("Debes ingresar algún texto");
+  }
 }
